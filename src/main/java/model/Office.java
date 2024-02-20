1edc3a58ec8343f32d0747c87ab2126c060bd31f
Eliseo Orellana <97992106+eliseoOrellana@users.noreply.github.com>
2024-02-20 00:09:44 -0300

    feat(class Office):Create Office class with attributes, constructor, and getters/setters

---
+++ src/main/java/model/Office.java
@@ -0,0 +1,47 @@
+package model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "offices")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Office {
+
+    @Id
+    @Column(name = "officeCode", length = 10)
+    private String officeCode;
+    
+    @Column(name = "city", length = 50)
+    private String city;
+    
+    @Column(name = "phone", length = 50)
+    private String phone;
+    
+    @Column(name = "addressLine1", length = 50)
+    private String addressLine1;
+    
+    @Column(name = "addressLine2", length = 50)
+    private String addressLine2;
+    
+    @Column(name = "state", length = 50)
+    private String state;
+    
+    @Column(name = "country", length = 50)
+    private String country;
+    
+    @Column(name = "postalCode", length = 15)
+    private String postalCode;
+    
+    @Column(name = "territory", length = 10)
+    private String territory;
+}
+
