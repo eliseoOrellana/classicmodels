c15f4dc27038ae78b782b4ba348dc6d842d84257
Eliseo Orellana <97992106+eliseoOrellana@users.noreply.github.com>
2024-02-20 00:12:10 -0300

    feat(class Employee):Create Employee class with attributes, constructor, and getters/setters

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,46 @@
+package model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @Column(name = "employeeNumber")
+    private int employeeNumber;
+    
+    @Column(name = "lastName", length = 50)
+    private String lastName;
+    
+    @Column(name = "firstName", length = 50)
+    private String firstName;
+    
+    @Column(name = "extension", length = 10)
+    private String extension;
+    
+    @Column(name = "email", length = 100)
+    private String email;
+    
+    @ManyToOne
+    @JoinColumn(name = "officeCode", referencedColumnName = "officeCode")
+    private Office office;
+    
+    @ManyToOne
+    @JoinColumn(name = "reportsTo", referencedColumnName = "employeeNumber")
+    private Employee manager;
+    
+    @Column(name = "jobTitle", length = 50)
+    private String jobTitle;
+}
