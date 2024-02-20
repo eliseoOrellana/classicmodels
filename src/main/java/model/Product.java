c54c6d5bdba53f8c13ea24f414aa87469c22dd3b
Eliseo Orellana <97992106+eliseoOrellana@users.noreply.github.com>
2024-02-19 23:56:47 -0300

    feat(product): add getter and setter

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -6,8 +6,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
     @Id
