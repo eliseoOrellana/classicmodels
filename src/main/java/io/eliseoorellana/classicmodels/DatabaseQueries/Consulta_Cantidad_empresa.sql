1d26904a974c2d5750e7cde34a2b895f81a0112e
Eliseo Orellana <97992106+eliseoOrellana@users.noreply.github.com>
2024-02-24 21:33:21 -0300

    feat(querys consulta_cantidad_empresa)

--- src/main/java/io/eliseoorellana/classicmodels/DatabaseQueries/Consulta_Cantidad_empresa.sql
+++ src/main/java/io/eliseoorellana/classicmodels/DatabaseQueries/Consulta_Cantidad_empresa.sql
@@ -3,3 +3,9 @@ inner join employees
 on employees.employeeNumber=customers.salesRepEmployeeNumber
 group by salesRepEmployeeNumber 
 order by Cant_Clientes desc ;
+
+select sales_rep_employee_number,employees.first_name,employees.last_name,count(sales_rep_employee_number) as Cant_Clientes from customers
+inner join employees
+on employees.employee_number=customers.sales_rep_employee_number
+group by sales_rep_employee_number 
+order by Cant_Clientes desc ;
