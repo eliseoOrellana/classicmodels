658d1b49e6a596a320219e7978f4d499ea018d38
Eliseo Orellana <97992106+eliseoOrellana@users.noreply.github.com>
2024-02-24 22:56:55 -0300

    feat templates

--- src/main/java/io/eliseoorellana/classicmodels/Controller/EmployeeController.java
+++ src/main/java/io/eliseoorellana/classicmodels/Controller/EmployeeController.java
@@ -1,6 +1,5 @@
 package io.eliseoorellana.classicmodels.Controller;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
