035be3d1276466a38de7d73a12c40c4c5780f7d1
Eliseo Orellana <97992106+eliseoOrellana@users.noreply.github.com>
2024-03-10 18:59:49 -0300

    feat(employee): Configure @Id field for auto-increment in Employee entity. Added the @GeneratedValue(strategy = GenerationType.IDENTITY) annotation to the id field in the Employee entity class to make it auto-incremental.

--- src/main/java/io/eliseoorellana/classicmodels/model/Employee.java
+++ src/main/java/io/eliseoorellana/classicmodels/model/Employee.java
@@ -2,6 +2,8 @@ package io.eliseoorellana.classicmodels.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -18,6 +20,7 @@ import lombok.NoArgsConstructor;
 public class Employee {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "employeeNumber")
     private Integer employeeNumber;
     
